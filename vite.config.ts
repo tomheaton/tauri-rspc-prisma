0cb35f8d83311169bfdf60dc7c6d670ed406b0c8
Tom Heaton <thomas.m.heaton@gmail.com>
2024-05-14 13:59:04 +0100

    cleanup versions

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(async () => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent vite from obscuring rust errors
