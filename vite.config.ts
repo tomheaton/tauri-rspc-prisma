f1d9cca4bc9feee0fd18084b4030e8975bfc371e
Tom Heaton <thomas.m.heaton@gmail.com>
2024-07-28 23:48:15 +0100

    new vite path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import react from "@vitejs/plugin-react";
-import path from "node:path";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
@@ -7,7 +7,7 @@ export default defineConfig(async () => ({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 
